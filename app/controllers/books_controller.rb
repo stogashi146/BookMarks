62dbe1cec8b7a73ba06772f70898aefcc0111120
stogashi146 <stogashi146@gmail.com>
2021-08-08 09:57:04 +0000

    [add] 本一覧ページ実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
+
   def index
+    @keyword = params[:keyword]
+    if @keyword && params[:page].nil?
+      @page = 1
+      @books = RakutenWebService::Books::Total.search(keyword: @keyword, booksGenreId: "001001", hits: 28).page(1)
+    elsif params[:page].present?
+      @page = params[:page].to_i
+      @books = RakutenWebService::Books::Total.search(keyword: @keyword, booksGenreId: "001001", hits: 28).page(params[:page])
+    end
+
+
+
   end
 
   def show
   end
 
   def create
+  end
+
+  def search
+
   end
 end
