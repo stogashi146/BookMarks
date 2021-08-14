e226c6858fe7a0a186ae5581c810d7e843bb59e0
stogashi146 <stogashi146@gmail.com>
2021-08-14 14:35:53 +0000

    [Fix]　不要なMVCを削除

--- app/controllers/book_reads_controller.rb
+++ app/controllers/book_reads_controller.rb
@@ -1,12 +1,6 @@
 class BookReadsController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @book = Book.find(params[:book_id])
-    @user = User.find(params[:user])
-    @book_read = @book.book_reads.find_by(user_id: @user.id)
-  end
-
   def create
     book = Book.find(params[:book_id])
     book_read = book.book_reads.new(read_params)
